03a0b59bee854a2da4721f6fc9a5da112be7ec58
joncodo <jonathan.carl.odonnell@gmail.com>
2016-08-11 13:28:37 -0300

    removing bad packages I no longer need

--- models/file.js
+++ models/file.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://0.0.0.0:27017/test');
 
 module.exports.create = function* create(options) {
+  // Create a file with the path and the html doc contents
+  // Expose api endpoint to read the html that is needed
+  // Call that endpoint from the angular website
+
+  // Deploy this
+  // Test the githubWebhook to create cats
+  // Test the githubWebhook to see which document needs to be updated in the DB
 
   const Cat = mongoose.model('Cat', { name: String });
 
