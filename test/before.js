8b28b08a9f16bf0f2ec239ca06546869501c3301
BenMacLean <benhmaclean@gmail.com>
2016-08-12 12:31:20 -0300

    DB Before clear working

--- test/before.js
+++ test/before.js
@@ -1,50 +1,23 @@
-console.log('top');
+console.log('DB Delete');
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://root:root@ds153775.mlab.com:53775/html';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected succesfully to server");
+before(function(done) {
 
-  db.close();
-});
+  MongoClient.connect(url, function(err, db) {
 
-describe("Customers", function(){
+    var col = db.collection('file');
+    col.deleteMany({}, function(err, r) {
+      assert.equal(null, err);
+      db.close();
+      done()
+    });
 
-before(function() {
+  });
 
-
-
-
-//
-//   console.log('before');
-//
-//   var mongodb = require('mongodb');
-//
-//   var MongoClient = mongodb.MongoClient;
-//
-//   var url = 'mongodb://root:root@ds153775.mlab.com:53775/html';
-//
-//   MongoClient.connect(url, function(err, db) {
-//     if (err) {
-//       console.log('Unable to connect to the mongoDB server. Error:', err);
-//     } else {
-//       console.log('Connection established to', url);
-//
-//       var clearDB = function(db, callback) {
-//         db.collection('file').deleteMany({}, function(err, results) {
-//           console.log('results', results);
-//           callback();
-//         });
-//       };
-//
-//       db.close();
-//     }
-//   });
-});
 });
